f1a1a0700c203007bc9da9429eb038e9fa7bf9eb
obed325 <obed325@hotmail.com>
2020-10-06 00:17:12 +0200

    Four models created

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebCommunity.Models;
 
 namespace WebCommunity.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }

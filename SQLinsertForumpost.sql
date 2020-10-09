055250a861ea8a07efe2886dccc6f0904f98d07b
obed325 <obed325@hotmail.com>
2020-10-09 20:35:28 +0200

    Insert one post by query

---
+++ SQLinsertForumpost.sql
@@ -0,0 +1,6 @@
+﻿select * from Posts
+
+insert into Posts (Content, Created,ForumId, Subject ,UserId)
+values ('First Ruby post -hello!', GETDATE(), 2,'First ruby post','3ff59e0c-0740-473c-936f-383c094df6df');
+
+select * from AspNetUsers

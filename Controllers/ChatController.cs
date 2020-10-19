d1a99fab0ea1cb77c64a3db0eb404e1d88d5be22
obed325 <obed325@hotmail.com>
2020-10-19 15:49:53 +0200

    char design

--- Controllers/ChatController.cs
+++ Controllers/ChatController.cs
@@ -2,19 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WebCommunity.Models;
 
 namespace WebCommunity.Controllers
 {
     public class ChatController : Controller
     {
+        private UserManager<ApplicationUser> userManager;
+
+        public ChatController(UserManager<ApplicationUser> usrMgr)
+        {
+            userManager = usrMgr;
+        }
         public IActionResult Index()
         {
             return View();
         }
-        public ActionResult Chat()
+        public IActionResult Chat()
         {
-            return View();
+            //var userName = userManager.GetUserAsync();
+            //ApplicationUser user = userManager.GetUserNameAsync();
+            //if (user != null)
+            //{
+            //    if (!string.IsNullOrEmpty(userName))
+            //        user.UserName = userName;
+
+            //    return View(user);
+            //}
+            //else
+                return View();
         }
     }
 }
